09e03ea5a0bfc5faa73f4d6ddf55aa191438464b
xian-wen <xianwen_yin@163.com>
2018-05-29 18:29:34 +0800

    add A1059

---
+++ Advanced/PAT_A1059 prime_factors.cpp
@@ -0,0 +1,23 @@
+/*
+1059 Prime Factors (25)
+时间限制: 100ms  内存限制: 64MB  代码长度限制: 16KB
+
+Decription:
+Given any positive integer N, you are supposed to find all of its prime factors, and write them in the format N = p~1~\^k~1~ * p~2~\^k~2~ *…*p~m~\^k~m~.
+
+Input Specification:
+Each input file contains one test case which gives a positive integer N in the range of long int.
+
+Output Specification:
+Factor N in the format N = p~1~\^k~1~ * p~2~\^k~2~ *…*p~m~\^k~m~, where p~i~'s are prime factors of N in increasing order, and the exponent k~i~ is the number of p~i~ -- hence when there is only one p~i~, k~i~ is 1 and must NOT be printed out.
+
+Sample Input:
+97532468
+
+Sample Output:
+97532468=2^2*11*17*101*1291
+ */
+ 
+ 
+ 
+ 
